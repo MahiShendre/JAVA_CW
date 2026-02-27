9bdf0d9c6074cffe886fffbb73d9fdb90ece5278
mahi <mahishendre40@gmail.com>
2026-02-27 14:53:24 +0530

    Program1 file is added

--- switch/Demo1.java
+++
@@ -1,8 +0,0 @@
-class FirstCodeDemo{
-    public static void main(String[] args ){
-        char ch= 'A';
-        int mahi = 2;
-        System.out.println(ch);
-        System.out.println(mahi);
-    }
-}
