8803f146d6f004d34e41e0f7c493e3b08eee04c4
mahi <mahishendre40@gmail.com>
2026-02-28 18:57:52 +0530

    Program7 file is added

---
+++ String/Program7.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Program7{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter string: ");
+        String str = sc.nextLine();     //better for total sentence
+
+        int count = 0;
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        System.out.println("Number of vowels: " + count);
+    }
+}
+
